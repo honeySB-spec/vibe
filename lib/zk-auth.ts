7c5384c5a092a9d138935054f9e21042690bb68d
Honey Singh Bisht <honey05sb@gmail.com>
2026-01-29 21:28:58 +0530

    ZKP auth error fixed

--- lib/zk-auth.ts
+++ lib/zk-auth.ts
@@ -1,5 +1,5 @@
 import { secp256k1 } from '@noble/curves/secp256k1.js';
-import { sha256 } from '@noble/hashes/sha256.js';
+import { sha256 } from '@noble/hashes/sha2.js';
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils.js';
 
 // Schnorr Identification Protocol (Non-Interactive via Fiat-Shamir)
